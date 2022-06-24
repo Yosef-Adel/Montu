bb76827cef09ecda716f0bd0b9231de4ca52620c
Yosef Adel <76741960+Yosef-Adel@users.noreply.github.com>
2022-06-24 19:29:40 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,25 @@ import Features from '../components/Features'
 import Layout from '../components/Layout'
 import Service from '../components/Service'
 import Work from '../components/Work'
-
+import { useRef } from 'react'
 const Home = () => {
+  const work = useRef(null); 
+  const clickHandeler = ()=>{
+    window.scrollTo({
+      top: work.current.offsetTop,
+      behavior: 'smooth'
+    })
+  }
   return (
     <div>
-        <Layout/>
+        <Layout clickHandeler= {clickHandeler}/>
         <Service/>
         <About/>
         <Features/>
+        <div ref={work}>
         <Work/>
+        </div>
+        
     </div>
   )
 }

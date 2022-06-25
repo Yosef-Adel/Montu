d1ecabb22eec6e0613982e55ce133d49a4da5417
Yosef Adel <76741960+Yosef-Adel@users.noreply.github.com>
2022-06-25 17:28:08 +0200

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,6 +5,8 @@ import img from '../img/img1.jpg'
 import img2 from '../img/img2.jpg'
 import img3 from '../img/img3.jpg'
 import img4 from '../img/img4.jpg'
+import img5 from '../img/img5.jpg'
+
 const About = () => {
   return (
     <div className={classes.aboutContainer}>
@@ -12,8 +14,10 @@ const About = () => {
       <AboutCard name = 'Omar Alaa' position = 'Front end Developer' img = {img2} />
       <AboutCard name = 'Oamr Nashat' position = 'Devops' img = {img3} />
       <AboutCard name = 'Abdelrahman Ezzat' position = 'back end Developer' img = {img4} />
+      <AboutCard name = 'Farah Mohammed' position = 'back end Developer' img = {img5} />
 
-     
+
+
 
     </div>
   )
